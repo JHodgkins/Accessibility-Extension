f1dbdbd86ff9f50d4adf022fcd04e6b320d098e4
JHodgkins <46275046+JHodgkins@users.noreply.github.com>
2024-12-24 11:48:54 +0000

    added contrast call

--- popup.js
+++ popup.js
@@ -1,11 +1,17 @@
-async function executeScript(action, checked) {
-    let [tab] = await chrome.tabs.query({active: true, currentWindow: true});
-    chrome.scripting.executeScript({
-      target: {tabId: tab.id},
-      files: ['content.js']
-    });
-    chrome.tabs.sendMessage(tab.id, {action: action, checked: checked});
-  }
+async function executeScript(action, checked, contrastLevel = null) {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    files: ['content.js']
+  });
+
+  chrome.tabs.sendMessage(tab.id, {
+    action: action,
+    checked: checked,
+    contrastLevel: contrastLevel
+  });
+}
   
   document.getElementById('showHeadings').addEventListener('change', (event) => {
     executeScript('toggleHeadings', event.target.checked);
@@ -15,3 +21,7 @@ async function executeScript(action, checked) {
     executeScript('toggleTabStops', event.target.checked);
   });
   
+  document.getElementById('contrastCheck').addEventListener('change', (event) => {
+    const contrastLevel = document.getElementById('contrastLevel').value;
+    executeScript('toggleContrast', event.target.checked, contrastLevel);
+  });
